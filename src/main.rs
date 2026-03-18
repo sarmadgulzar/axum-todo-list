977278f822e0839334ce8a230b5b1f0cddf43bed
Sarmad Gulzar <sarmadgulzar@icloud.com>
2026-03-18 23:29:04 +0500

    Add Axum web server with hello world endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::Ipv4Addr;
+
+use axum::Router;
+use axum::routing;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", routing::get(|| async { "Hello world!" }));
+
+    let addr = Ipv4Addr::new(127, 0, 0, 1);
+    let port = 8080;
+    let listner = TcpListener::bind((addr, port)).await.unwrap();
+
+    axum::serve(listner, app).await.unwrap();
 }
