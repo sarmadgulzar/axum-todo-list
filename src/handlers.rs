429cac06de663276d181a268c0176b4341b4a2fc
Sarmad Gulzar <sarmadgulzar@icloud.com>
2026-03-18 23:46:53 +0500

    Add health check endpoint to router

--- src/handlers.rs
+++ src/handlers.rs
@@ -3,3 +3,7 @@ use axum::response::IntoResponse;
 pub async fn root() -> impl IntoResponse {
     "Hello, world"
 }
+
+pub async fn health() -> impl IntoResponse {
+    "Ok"
+}
--- src/router.rs
+++ src/router.rs
@@ -3,5 +3,7 @@ use axum::{Router, routing};
 use crate::handlers;
 
 pub fn create_router() -> Router {
-    Router::new().route("/", routing::get(handlers::root))
+    Router::new()
+        .route("/", routing::get(handlers::root))
+        .route("/health", routing::get(handlers::health))
 }
